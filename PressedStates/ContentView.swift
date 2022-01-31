76ba66464cad2470c7c5edcedcff4b0e669df5bc
Philip Davis <philipcdavis11@gmail.com>
2022-01-31 10:49:10 -0800

    Initial Commit

---
+++ PressedStates/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PressedStates
+//
+//  Created by Philip Davis on 1/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PressedStates/PressedStatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PressedStatesApp.swift
+//  PressedStates
+//
+//  Created by Philip Davis on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PressedStatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
